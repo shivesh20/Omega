3d3f2027188f861527c9bc2e0da82b5839635777
shivesh20 <69833172+shivesh20@users.noreply.github.com>
2020-08-18 09:11:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCUBzsQFOuSExxAgWZ7vEX8bDpDIVbOKtA",
+  authDomain: "pes-2020-5000-myclub-coins.firebaseapp.com",
+  databaseURL: "https://pes-2020-5000-myclub-coins.firebaseio.com",
+  projectId: "pes-2020-5000-myclub-coins",
+  storageBucket: "pes-2020-5000-myclub-coins.appspot.com",
+  messagingSenderId: "866180921841",
+  appId: "1:866180921841:web:cd4e7696b36a765b42ec50",
+  measurementId: "G-JW4JS73YE3"
 };
 
      
